dd1bc976128b4f749b39d1e69cfe210ed1ec18b7
Julie <>
2021-02-05 10:01:13 +0000

    Initial commit

---
+++ src/regexplay/RegExPlay.java
@@ -0,0 +1,17 @@
+package regexplay;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegExPlay {
+
+    public static void main(String[] args) {
+
+        String pattern = "\\d{1,3}.?\\d{0,3}\\s[a-zA-Z]{2,30}\\s[a-zA-Z]{2,15}";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m= p.matcher("1 Beech avenue");
+        System.out.println(m.matches());
+
+    }
+
+}
