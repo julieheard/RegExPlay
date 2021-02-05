ebab13cae002efad2ec6ef1d6215f60cbceae520
Julie Heard <55280278+julieheard@users.noreply.github.com>
2021-02-05 10:06:32 +0000

    Comments

--- src/regexplay/RegExPlay.java
+++ src/regexplay/RegExPlay.java
@@ -7,6 +7,7 @@ public class RegExPlay {
 
     public static void main(String[] args) {
 
+        //Pattern for an address
         String pattern = "\\d{1,3}.?\\d{0,3}\\s[a-zA-Z]{2,30}\\s[a-zA-Z]{2,15}";
         Pattern p = Pattern.compile(pattern);
         Matcher m= p.matcher("1 Beech avenue");
